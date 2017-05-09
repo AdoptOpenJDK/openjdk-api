6fe4b883a4924bacdb239c0842c1cb2f940f568d
Joe-Brady <joe.brady@uk.ibm.com>
2017-05-09 13:49:43 +0100

    Serve README as webroot

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const RateLimit = require('express-rate-limit');
+const mds = require('markdown-serve');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -11,11 +13,26 @@ var limiter = new RateLimit({
   message: "You have exceeded your api usage, you are allowed 100 requests per hour"
 });
 
-//  apply to all requests
+// apply to all requests
 app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/routes')(app, {});
 app.listen(port, () => {
   console.log('We are live on ' + port);
+});
+
+
+// markdown serving
+app.set('views', path.resolve(__dirname, './markdown-layouts'));
+app.set('view engine', 'pug');
+app.use(express.static(path.resolve(__dirname, './markdown-layouts')));
+
+app.use('/', mds.middleware({
+    rootDirectory: path.resolve(__dirname, ''),
+    view: 'layout'
+}));
+
+app.get('/', function(req, res){
+  res.redirect('./README');
 });
