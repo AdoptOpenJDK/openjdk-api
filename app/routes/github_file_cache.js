e827bea9eb12e9a80c0def7c666e699822fc7cdb
John Oliver <johno@insightfullogic.com>
2018-07-19 14:08:26 +0100

    Use the slightly more features Q promise. Stop using github api due to rate limit.

--- app/routes/github_file_cache.js
+++ app/routes/github_file_cache.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const _ = require('underscore');
+const Q = require('q');
 
 const newCache = {};
 const oldCache = {};
@@ -18,129 +19,99 @@ function formErrorResponse(error, response, body) {
 // 3. If file is not modified return cached value
 // 4. If modified return new data and add it to the cache
 function cachedGet(url, cache) {
-  return new Promise(function (resolve, reject) {
-    const options = {
-      url: url,
-      headers: {
-        'User-Agent': 'adoptopenjdk-admin openjdk-api'
-      }
-    };
+  var deferred = Q.defer();
 
-    if (cache.hasOwnProperty(options.url)) {
-      console.log("cache property present")
-
-      if (Date.now() - cache[options.url].cacheTime < 120000) {
-        // For a given file check at most once every 2 min
-        console.log("cache hit cooldown");
-        resolve(cache[options.url].body);
-        return
-      } else {
-        // Ask github to only return data if the file was modified since,
-        // if not will return a 304
-        options.headers['If-Modified-Since'] = cache[options.url].modifiedTime
-      }
+  const options = {
+    url: url,
+    headers: {
+      'User-Agent': 'adoptopenjdk-admin openjdk-api'
     }
+  };
 
+  if (cache.hasOwnProperty(options.url)) {
+    console.log("cache property present")
+
+    if (Date.now() - cache[options.url].cacheTime < 120000) {
+      // For a given file check at most once every 2 min
+      console.log("cache hit cooldown");
+      deferred.resolve(cache[options.url].body);
+    }
+  } else {
     console.log("Checking " + options.url);
     request(options, function (error, response, body) {
       if (error !== null) {
-        reject(formErrorResponse(error, response, body));
+        deferred.reject(formErrorResponse(error, response, body));
         return;
       }
 
-      if (response.statusCode === 304) {
-        // File has not been modified, return the cached version
-        console.log("cache hit " + cache[options.url].modifiedTime);
-
-        // Reset cache cooldown
-        cache[options.url].cacheTime = Date.now();
-
-        resolve(cache[options.url].body);
-      } else if (response.statusCode === 200) {
+      if (response.statusCode === 200) {
         cache[options.url] = {
           cacheTime: Date.now(),
-          modifiedTime: response.headers['last-modified'],
-          body: JSON.parse(Buffer.from(JSON.parse(body).content, 'base64'))
+          body: JSON.parse(body)
         };
-        resolve(cache[options.url].body)
+        deferred.resolve(cache[options.url].body)
       } else {
-        reject(formErrorResponse(error, response, body));
+        deferred.reject(formErrorResponse(error, response, body));
       }
     });
-  });
+  }
+  return deferred.promise;
 }
 
 function getInfoForNewRepo(version, releaseType) {
-  return cachedGet(`https://api.github.com/repos/AdoptOpenJDK/${version}-binaries/contents/${releaseType}.json`, newCache);
+  return cachedGet(`https://raw.githubusercontent.com/AdoptOpenJDK/${version}-binaries/master/${releaseType}.json`, newCache);
 }
 
 function getInfoForOldRepo(version, releaseType) {
-  return new Promise(function (resolve, reject) {
-    cachedGet(`https://api.github.com/repos/AdoptOpenJDK/${version}-${releaseType}/contents/${releaseType}.json`, oldCache)
-      .then(function (hotspotBody) {
+  let deferred = Q.defer();
 
-        if (version.indexOf('amber') > 0) {
-          // if amber do not look at openj9 repo
-          resolve(hotspotBody);
-          return;
-        }
+  let hotspotPromise = cachedGet(`https://raw.githubusercontent.com/AdoptOpenJDK/${version}-${releaseType}/master/${releaseType}.json`, oldCache);
+  let openj9Promise;
 
-        cachedGet(`https://api.github.com/repos/AdoptOpenJDK/${version}-openj9-${releaseType}/contents/${releaseType}.json`, oldCache)
-          .then(function (openj9Body) {
-            const unifiedJson = _.union(hotspotBody, openj9Body);
-            resolve(unifiedJson);
-          })
-          .catch(function (error) {
-            reject(error);
-          })
-      })
-      .catch(function (error) {
-        reject(error);
-      });
-  });
+  if (version.indexOf('amber') > 0) {
+    openj9Promise = Q.fcall(function () {
+      return {}
+    });
+  } else {
+    openj9Promise = cachedGet(`https://raw.githubusercontent.com/AdoptOpenJDK/${version}-openj9-${releaseType}/master/${releaseType}.json`, oldCache);
+  }
+
+  Q.allSettled([hotspotPromise, openj9Promise])
+    .then(function (results) {
+      let hotspotResult = results[0];
+      let openj9Result = results[1];
+      if (hotspotResult.state !== "fulfilled" && openj9Result.state !== "fulfilled") {
+        deferred.reject(hotspotResult.reason);
+      } else {
+        let hotspotData = hotspotResult.state === "fulfilled" ? hotspotResult.value : [];
+        let openj9Data = openj9Result.state === "fulfilled" ? openj9Result.value : [];
+
+        const unifiedJson = _.union(hotspotData, openj9Data);
+        deferred.resolve(unifiedJson);
+      }
+    });
+
+  return deferred.promise;
 }
 
+
 exports.getInfoForVersion = function (version, releaseType) {
-  return new Promise(function (resolve, reject) {
+  let deferred = Q.defer();
 
-    var newData = undefined;
-    var oldData = undefined;
-    var newError = undefined;
-    var oldError = undefined;
+  Q.allSettled([getInfoForOldRepo(version, releaseType), getInfoForNewRepo(version, releaseType)])
+    .then(function (results) {
+      let oldData = results[0];
+      let newData = results[1];
 
-    function checkComplete() {
-      if (oldData !== undefined && newData !== undefined) {
-        if (newError !== undefined && oldError !== undefined) {
-          reject(oldError);
-        } else {
-          resolve({
-            newData: newData,
-            oldData: oldData
-          })
-        }
+      if (oldData.state !== "fulfilled" && newData.state !== "fulfilled") {
+        deferred.reject(oldData.reason);
+      } else {
+        let oldD = oldData.state === "fulfilled" ? oldData.value : [];
+        let newD = newData.state === "fulfilled" ? newData.value : [];
+
+        const unifiedJson = _.union(oldD, newD);
+        deferred.resolve(unifiedJson);
       }
-    }
-
-    getInfoForNewRepo(version, releaseType)
-      .then(function (data) {
-        newData = data;
-        checkComplete();
-      })
-      .catch(function (error) {
-        newData = null;
-        newError = error;
-        checkComplete();
-      });
-
-    getInfoForOldRepo(version, releaseType)
-      .then(function (data) {
-        oldData = data;
-        checkComplete();
-      })
-      .catch(function (error) {
-        oldData = null;
-        oldError = error;
-        checkComplete();
-      });
-  });
+    });
+  return deferred.promise;
 };
