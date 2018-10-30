8d32996822760daafe4cddbc60137dc061f51e07
Greg Parris <greg.parris@gmail.com>
2018-10-29 20:18:59 -0500

    Remove unnecessary 'express-routes-versioning' and simplify routes.
    * Due to route prefixing use of 'express-routes-versioning' makes no sense.
    * Use optional named route parameters to clean up API routes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,76 +1,31 @@
-const routesVersioning = require('express-routes-versioning')();
-const v1 = require('./v1');
-const v2 = require('./v2');
-
-// add future versions here. Also add to the routesVersioning object below.
-
 module.exports = function (app) {
   app.use((req, res, next) => {
     app.set('json spaces', req.query.pretty === 'false' ? false : 2);
     next();
   });
 
+  // API version 2
+  // Examples:
+  //      /info/releases/openjdk8
+  //      /info/nightly/openjdk8
+  //      /info/nightly/openjdk8?openjdk_impl=hotspot&os=windows&arch=x64
+  //      /binary/releases/openjdk8
+  //      /binary/nightly/openjdk8
+  //
+  // Optional query parameters:
+  //  openjdk_impl ::= "hotspot" | "openj9"
+  //  os ::= "windows" | "linux"
+  //  arch ::= "x64" | "x32" | "ppc64"
+  //  release ::= "latest"| <jdk_version>
+  //  type ::= "jdk" | "jre"
+  //
+  // curl "http://127.0.0.1:3000/v2/binary/nightly/openjdk8?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jdk"
+  // curl "http://127.0.0.1:3000/v2/info/releases/openjdk10?openjdk_impl=hotspot&type=jdk"
+  app.get('/v2/:requestType?/:buildtype?/:version?', require('./v2'));
+
+  // API version 1
   app.get([
-      //these 3 paths dont make sense and are not valid, but if you dont have them then the matching will fall through to the v1 api
-      '/v2',
-      '/v2/:requestType',
-      '/v2/:requestType/:buildtype',
-
-
-
-      '/v2/:requestType/:buildtype/:version'],
-
-    // Examples:
-    //      /info/releases/openjdk8
-    //      /info/nightly/openjdk8
-    //      /info/nightly/openjdk8?openjdk_impl=hotspot&os=windows&arch=x64
-
-    //      /binary/releases/openjdk8
-    //      /binary/nightly/openjdk8
-
-    // optional query parameters:
-    //
-    //  openjdk_impl ::= "hotspot" | "openj9"
-    //  os ::= "windows" | "linux"
-    //  arch ::= "x64" | "x32" | "ppc64"
-    //  release ::= "latest"| <jdk_version>
-    //  type ::= "jdk" | "jre"
-    //
-    // curl "http://127.0.0.1:3000/v2/binary/nightly/openjdk8?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jdk"
-    // curl "http://127.0.0.1:3000/v2/info/releases/openjdk10?openjdk_impl=hotspot&type=jdk"
-    routesVersioning({
-      '^2.0.0': v2
-    })
-  ).get([
-      '/v1/:variant',
-      '/v1/:variant/:buildtype',
-      '/v1/:variant/:buildtype/:platform',
-      '/v1/:variant/:buildtype/:platform/:build',
-      '/v1/:variant/:buildtype/:platform/:build/:datatype',
-
-      //Maintain backwards compatibility for a while
-      '/:variant',
-      '/:variant/:buildtype',
-      '/:variant/:buildtype/:platform',
-      '/:variant/:buildtype/:platform/:build',
-      '/:variant/:buildtype/:platform/:build/:datatype',
-    ],
-    routesVersioning({
-      '^1.0.0': v1
-      // add future versions here, with a comma ( , ) after the previous line. Also add the require(); above.
-    })
-  );
-
-
-  /*
-
-    if future versions add more routes, create a new 'app.get' here with that route, using either
-    the caret ( ^ ) or tilde ( ~ ) symbol (see npm version rules) to only allow usage of certain
-    versions when using that route, e.g:
-
-    app.get(['/newroute'], routesVersioning({
-      '^8.0.0': v8
-    }));
-
-  */
+    '/v1/:variant/:buildtype?/:platform?/:build?/:datatype?',
+    '/:variant/:buildtype?/:platform?/:build?/:datatype?' // Maintain backwards compatibility for a while
+  ], require('./v1'));
 };
