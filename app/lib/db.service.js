7848c1a8b3955de649bbd1949c4109f262a2a377
Parker Mauney <parkermauney@gmail.com>
2020-11-11 16:04:54 -0600

    Generate Mongo connection string from env vars if set

--- app/lib/db.service.js
+++ app/lib/db.service.js
@@ -1,19 +1,38 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const dbName = 'adoptOpenJdkApi';
 const collectionName = 'v2Stats';
-const dbuser = encodeURIComponent('dbuser');
-const dbpassword = encodeURIComponent('dbpassword');
+const dbName = process.env.MONGODB_DBNAME ? process.env.MONGODB_DBNAME : 'api-data';
+const url = createConnectionString();
 
-const url = `mongodb+srv://${dbuser}:${dbpassword}@cluster0-fdnyp.mongodb.net/test?retryWrites=true&w=majority`;
-
-/**
- * @const {MongoClientOptions}
- */
+/** @const {MongoClientOptions} */
 const options = {
   useUnifiedTopology: true,
 };
 
+/**
+ * Generate a connection string from env vars.
+ * If no such env vars are set, this will point to localhost:27017 by default.
+ *
+ * @return {string}
+ */
+function createConnectionString() {
+  const dbUser = process.env.MONGODB_USER ? encodeURIComponent(process.env.MONGODB_USER) : null;
+  const dbPassword = process.env.MONGODB_PASSWORD ? encodeURIComponent(process.env.MONGODB_PASSWORD) : null;
+  const dbHost = process.env.MONGODB_HOST ? process.env.MONGODB_HOST : 'localhost';
+  const dbPort = process.env.MONGODB_PORT ? process.env.MONGODB_PORT : '27017';
+
+  if (dbUser && dbPassword) {
+    return `mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+  } else {
+    let dbServerSelectionTimeout = process.env.MONGODB_SERVER_SELECTION_TIMEOUT_MILLIS;
+    if (!dbServerSelectionTimeout) {
+      dbServerSelectionTimeout = '100';
+    }
+    return `mongodb://${dbHost}:${dbPort}/?serverSelectionTimeoutMS=${dbServerSelectionTimeout}`
+  }
+}
+
+/** @type {MongoClient} */
 let connection = null;
 
 /**
--- test/lib/db.service.test.js
+++ test/lib/db.service.test.js
@@ -2,16 +2,22 @@ const {MongoClient} = require('mongodb');
 
 describe('DB service', () => {
   let dbService;
+  let urlSpy;
+  let optionsSpy;
 
   beforeEach(() => {
+    urlSpy = null;
+    optionsSpy = null;
+
     jest.setMock('mongodb', {
       MongoClient: {
         connect: function (url, options, callback) {
+          urlSpy = url;
+          optionsSpy = options;
           return callback(null, MongoClient.prototype);
         }
       }
     });
-    dbService = require('../../app/lib/db.service');
   });
 
   afterEach(() => {
@@ -21,10 +27,78 @@ describe('DB service', () => {
   it('throws error when attempting to get client before connecting', () => {
     expect.assertions(3);
 
+    dbService = require('../../app/lib/db.service');
+
     expect(() => dbService.get()).toThrowError();
     return dbService.connect().then(res => {
       expect(() => dbService.get()).not.toThrowError();
       expect(dbService.get()).toEqual(res);
+    });
+  });
+
+  describe('connection config', () => {
+    const dbName = 'some-db-name';
+    const dbUser = 'some-db-user';
+    const dbPassword = 'some-db-password';
+    const dbHost = 'some-db-host';
+    const dbPort = 'some-db-port';
+    const dbServerSelectionTimeout = 12345;
+
+    beforeEach(() => {
+      process.env.MONGODB_DBNAME = dbName;
+      process.env.MONGODB_USER = dbUser;
+      process.env.MONGODB_PASSWORD = dbPassword;
+      process.env.MONGODB_HOST = dbHost;
+      process.env.MONGODB_PORT = dbPort;
+      process.env.MONGODB_SERVER_SELECTION_TIMEOUT_MILLIS = dbServerSelectionTimeout.toString(10);
+    });
+
+    afterEach(() => {
+      delete process.env.MONGODB_DBNAME;
+      delete process.env.MONGODB_USER;
+      delete process.env.MONGODB_PASSWORD;
+      delete process.env.MONGODB_HOST;
+      delete process.env.MONGODB_PORT;
+      delete process.env.MONGODB_SERVER_SELECTION_TIMEOUT_MILLIS;
+    });
+
+    it('loads connection info from env vars', () => {
+      expect.assertions(2);
+      dbService = require('../../app/lib/db.service');
+
+      return dbService.connect().then(() => {
+        expect(urlSpy).toEqual(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`);
+        expect(optionsSpy).toEqual({
+          useUnifiedTopology: true,
+        });
+      });
+    });
+
+    it('defaults to local connection string if no vars are set', () => {
+      delete process.env.MONGODB_DBNAME;
+      delete process.env.MONGODB_USER;
+      delete process.env.MONGODB_PASSWORD;
+      delete process.env.MONGODB_HOST;
+      delete process.env.MONGODB_PORT;
+      delete process.env.MONGODB_SERVER_SELECTION_TIMEOUT_MILLIS;
+
+      expect.assertions(1);
+      dbService = require('../../app/lib/db.service');
+
+      return dbService.connect().then(() => {
+        expect(urlSpy).toEqual('mongodb://localhost:27017/?serverSelectionTimeoutMS=100');
+      });
+    });
+
+    it('uses default DB name if not set', () => {
+      delete process.env.MONGODB_DBNAME;
+
+      expect.assertions(1);
+      dbService = require('../../app/lib/db.service');
+
+      return dbService.connect().then(() => {
+        expect(urlSpy).toEqual(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/api-data`);
+      });
     });
   });
 });
